318a71f2039c9d1dbcf34907688f1e62869b4196
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-04-10 14:00:49 -0300

    Update bar.php

--- templates/bar.php
+++ templates/bar.php
@@ -1,12 +1,12 @@
 <?php if (!$data) return; ?>
 
-<div id="fsb-bar" class="fsb-bar-wrapper">
+<div id="fsb-bar" class="fsb-bar-wrapper" style="margin-bottom: 5px">
   <?php if ($data['remaining'] > 0): ?>
     <div class="fsb-bar fsb-warning">
       FALTAM <strong><?php echo wc_price($data['remaining']); ?></strong> PARA PORTES GRÁTIS!
     </div>
   <?php else: ?>
-    <div class="fsb-bar fsb-success">
+    <div class="fsb-bar fsb-success" style="margin-bottom: 5px">
       🎉 VOCÊ GANHOU FRETE GRÁTIS!
     </div>
   <?php endif; ?>
